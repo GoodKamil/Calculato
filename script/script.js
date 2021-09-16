11d2bd06cfbb7c7712a242056238d9e42fed3eab
Kamil Krawczak <Kamil.Krawczak@onet.eu>
2021-09-16 20:51:33 +0200

    finally project

--- script/script.js
+++ script/script.js
@@ -6,12 +6,43 @@ const btnDelete = document.querySelector('[data-option="delete"]');
 const btnBack = document.querySelector('[data-option="back"]');
 const paragraph = document.querySelector('.paragraph');
 
+let numberClick = '';
 let firstNumber = '';
 let secondNumber = '';
 let action = '';
 let result = '';
 let value = '';
+let sum = '';
 let flag = true;
+
+function condition(char, number) {
+  switch (char) {
+    case 'x':
+      score = number * secondNumber;
+      break;
+    case '-':
+      score = number - secondNumber;
+      break;
+    case '+':
+      score = number + secondNumber;
+      break;
+    case '/':
+      score = number / secondNumber;
+      break;
+
+    default:
+      return;
+  }
+  return score;
+}
+
+function clear(text) {
+  textResult.textContent = text;
+  firstNumber = '';
+  secondNumber = '';
+  paragraph.textContent = '';
+  flag = !flag;
+}
 
 function remove() {
   paragraph.textContent = '';
@@ -20,7 +51,9 @@ function remove() {
   secondNumber = '';
   action = '';
   result = '';
+  numberClick = '';
   value = '';
+  sum = '';
   flag = true;
 }
 
@@ -30,15 +63,20 @@ btnNumbers.forEach(number => {
       textResult.textContent = '';
       result = '';
     }
-    textResult.textContent += number.textContent;
+
+    if (sum && flag) {
+      textResult.textContent = '';
+      sum = '';
+    }
+
+    numberClick = number.textContent;
+    textResult.textContent += numberClick;
     value = Number(textResult.textContent);
     if (flag == true) {
       paragraph.textContent = '';
       firstNumber = value;
-      console.log(firstNumber);
     } else {
       secondNumber = value;
-      console.log(secondNumber);
     }
   });
 });
@@ -47,36 +85,27 @@ btnOperations.forEach(operation => {
   operation.addEventListener('click', function () {
     textResult.textContent = '';
     action = String(operation.textContent);
-    paragraph.textContent = `${value}${action}`;
+    paragraph.textContent = sum ? `${sum}${action}` : `${value}${action}`;
     flag = !flag;
   });
 });
 
 btnResult.addEventListener('click', function () {
   if (firstNumber && secondNumber && action) {
-    switch (action) {
-      case 'x':
-        result = firstNumber * secondNumber;
-        break;
-      case '-':
-        result = firstNumber - secondNumber;
-        break;
-      case '+':
-        result = firstNumber + secondNumber;
-        break;
-      case '/':
-        result = firstNumber / secondNumber;
-        break;
-
-      default:
-        return;
-    }
-    textResult.textContent = result;
-    paragraph.textContent = '';
-    flag = !flag;
-    firstNumber = '';
-    secondNumber = '';
+    result = condition(action, firstNumber);
+    clear(result);
+    sum = result;
+  } else if (secondNumber && sum && action) {
+    sum = condition(action, sum);
+    clear(sum);
   } else console.log('błąd');
 });
 
 btnDelete.addEventListener('click', remove);
+
+btnBack.addEventListener('click', function () {
+  // const leng = textResult.textContent.length - 1;
+  const texts = textResult.textContent;
+  console.log(texts);
+  textResult.textContent = texts.slice(0, texts.length - 1);
+});
